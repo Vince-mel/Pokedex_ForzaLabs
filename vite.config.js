a83b396267523e630209180eee7b82919adcdace
Vincenzo Melillo <137229406+Vince-mel@users.noreply.github.com>
2024-05-26 18:56:55 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
-// vite.config.js
-export default {
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {},
+  },
+  server: {
+    // Configure your development server here
+    port: 3000, // Default port is 3000
+    open: true, // Open the browser automatically on startup
+  },
   build: {
+    // Configure your production build here
+    outDir: "dist", // Default output directory is 'dist'
     rollupOptions: {
-      external: ["react-icons/fa"],
+      // Configure Rollup options here
+      external: ["react-icons/fa"], // Add external dependencies here if needed
     },
   },
-};
+});
