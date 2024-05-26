fbf2cae480fb6fc1facc36c15800c43444478695
Vincenzo Melillo <137229406+Vince-mel@users.noreply.github.com>
2024-05-26 18:52:32 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Pokedex from "./components/Pokedex";
 import Pokemon from "./components/Pokemon";
