0f5e49faca0bc8d5c66d872083a554b478f09799
Vincenzo Melillo <137229406+Vince-mel@users.noreply.github.com>
2024-05-26 17:45:30 +0200

    .

--- pokedex/src/main.jsx
+++ pokedex/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
 import { MantineProvider } from "@mantine/core";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
