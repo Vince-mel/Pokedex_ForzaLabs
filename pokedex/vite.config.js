ecd519a3902f3f005b947144ff998002317ae23b
Vincenzo Melillo <137229406+Vince-mel@users.noreply.github.com>
2024-05-26 17:47:02 +0200

    .

--- pokedex/vite.config.js
+++ pokedex/vite.config.js
@@ -1,7 +1,17 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["@mantine/core"],
+    },
+  },
 });
